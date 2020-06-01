29b91d61eaf3ca8499a9a3113bb16f92c216cda4
Cody Collier <cody@telnet.org>
2020-06-01 01:25:34 -0500

    bug fixing for b77

--- _processors/process-polyai-tsd-banking77.sh
+++ _processors/process-polyai-tsd-banking77.sh
@@ -21,15 +21,36 @@ curl -s -o "${original1}" -C - "https://raw.githubusercontent.com/PolyAI-LDN/tas
 curl -s -o "${original2}" -C - "https://raw.githubusercontent.com/PolyAI-LDN/task-specific-datasets/master/banking_data/test.csv"
 
 # Process
+# This file has "" around phrases with commas, and no quotes around thouse without. Therefore a 2 pass process helps.
 cat ${original1} ${original2} | # ...
     grep -v "^text,category"  | # Remove the header
+    grep '^"'                 | # Get the phrases with pre-existing quotes (pass 1)
+    grep ","                  | # Remove lines without a comma (empy, " only, missing intent)
+    grep -v '^",'             | # Remove malformed lines
+    grep -v "but I want to transfer some of the money to my account here.$" | # Remove malformed line with no intent 
+    grep -v "I think someone managed to get my card details and use it.$" | # Remove malformed line with no intent / duplicate
+    sed 's/""//g'             | # Remove the misc ""
+    sed 's/|//g'              | # Remove the one errant |
     sed 's///g'             | # Remove the windows newlines in this set
-    sed 's/"//g'              | # Remove all the quotes in the text (inconsistent quoting)
-    sed 's/^./"&/g'           | # Now re-add proper csv quoting
-    sed 's/$/"/g'             | # ...
-    sed 's/,/","/g'           | # ...
-    sed 's/"," /, /g'         | # Don't quote commas inside of the utterance text
+    sed 's/",/","/g'          | # Add a quote at the front of the intent after the comma
+    sed 's/$/"/g'             | # Add a " at the end
     sed "s/^/\"${slug}\",/g"  | # Add slug as col 1
     cat > ${processed}
+
+cat ${original1} ${original2} | # ...
+    grep -v "^text,category"  | # Remove the header
+    grep -v '^"'              | # Get the phrases withOUT pre-existing quotes (pass 2)
+    grep ","                  | # Remove lines without a comma (empy, " only, missing intent)
+    grep -v '^",'             | # Remove malformed lines
+    grep -v "but I want to transfer some of the money to my account here.$" | # Remove malformed line with no intent 
+    grep -v "I think someone managed to get my card details and use it.$" | # Remove malformed line with no intent / duplicate
+    sed 's/""//g'             | # Remove the misc ""
+    sed 's/|//g'              | # Remove the one errant |
+    sed 's///g'             | # Remove the windows newlines in this set
+    sed 's/^./"&/g'           | # Add quotes at front (why . + & -> byte sequence err)
+    sed 's/,/","/g'           | # Add a quotes before and after the separating comma
+    sed 's/$/"/g'             | # Add a " at the end
+    sed "s/^/\"${slug}\",/g"  | # Add slug as col 1
+    cat >> ${processed}
 
 
