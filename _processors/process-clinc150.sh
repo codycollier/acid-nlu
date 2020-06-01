c3a8defde502de46b2f3232a618efdcb5264301d
Cody Collier <cody@telnet.org>
2020-05-31 19:26:32 -0500

    add processor for clinc150

---
+++ _processors/process-clinc150.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Retrieve and process the clinc150 set
+#
+#
+
+slug="clinc150"
+
+cd $(dirname $0)
+
+# Make a workspace
+mkdir -p 1-original/
+mkdir -p 2-processed/
+
+original="./1-original/${slug}.data_full.json"
+processed="./2-processed/${slug}.csv"
+
+# Retrieve the full set
+curl -s -o "${original}" -C - "https://raw.githubusercontent.com/clinc/oos-eval/master/data/data_full.json"
+
+# Process
+cat ${original}                              | # ... 
+    jq -r '[.val, .train, .test][][] | @csv' |  # Grab only the non oos labeled intent data 
+    sed 's/""//g'                            |  # Remove the inconsistent double double quotes
+    sed "s/^/\"${slug}\",/g"                 |  # Add slug as col 1
+    cat > ${processed}
